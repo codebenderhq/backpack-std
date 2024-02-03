72cf2dc43f62506a530fb5bfacb0da6820ecb753
Rawk Akani <lionelmbankeu@gmail.com>
2024-02-03 08:54:19 +0000

    well what do we have here

--- src/middleware/script.js
+++ src/middleware/script.js
@@ -8,7 +8,7 @@ const script_middleware = async (req, isProd) => {
   let res = await import(`file:///${app_path}${_pathname}`);
 
   // this is to support deployment to a linux enviroment
-  if (!Deno.build.os === "windows" && isProd) {
+  if (Deno.build.os !== "windows" && isProd) {
     res = await import(`app/${app_path}${_pathname}`);
   }
 
