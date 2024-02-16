ee64e3fc055624166b9be97c9d2a1bbdeca65385
Rawk Akani <lionelmbankeu@gmail.com>
2024-02-16 14:22:26 +0000

    adding contrainst

--- src/middleware/asset.js
+++ src/middleware/asset.js
@@ -1,10 +1,10 @@
 import { serveFile } from "jsr:@std/http@0.216.0/file_server";
 // https://postcss.org/api/
 import postcss from "npm:postcss@latest";
-import autoprefixer from "npm:autoprefixer";
-import tailwindcss from "npm:tailwindcss";
+import autoprefixer from "npm:autoprefixer@latest";
+import tailwindcss from "npm:tailwindcss@latest";
 // https://lightningcss.dev/docs.html
-import { transform } from "npm:lightningcss";
+import { transform } from "npm:lightningcss@latest";
 
 const asset_middlware = async (request, type) => {
   const _cwd = window._cwd;
